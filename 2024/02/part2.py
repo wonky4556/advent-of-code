dc6f527678e82726c018d65d4b7474b0574a5eda
Aishwar <aishwar.sabesan@gmail.com>
2024-12-02 13:59:22 -0500

    day 2 solution

---
+++ 2024/02/part2.py
@@ -0,0 +1,31 @@
+#read input 
+file = open("day2_input.txt", "r")
+content = file.readlines()
+file.close()
+
+reports = []
+for line in content:
+    reports.append([int(n) for n in line.strip().split()])
+
+def isDirectional(report):
+    is_ascending = all(report[i] < report[i + 1] for i in range(len(report) - 1))
+    is_descending = all(report[i] > report[i + 1] for i in range(len(report) - 1))
+    return is_ascending or is_descending
+
+def isWithinTolerance(report):
+    diffs = [abs(report[i] - report[i + 1]) for i in range(len(report) - 1)]
+    return all([diff >= 1 and diff <= 3 for diff in diffs])
+
+count = 0
+for report in reports:
+    if isDirectional(report) and isWithinTolerance(report):
+        count = count + 1
+    else:
+        for i in range(len(report)):
+            newReport = report.copy()
+            del newReport[i]
+            if isDirectional(newReport) and isWithinTolerance(newReport):
+                count = count + 1
+                break
+
+print(count)
