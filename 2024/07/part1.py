f98e2493b0071a94da766e99d8a72e6d49106107
Aishwar <aishwar.sabesan@gmail.com>
2024-12-12 10:37:49 -0500

    day7 solution

---
+++ 2024/07/part1.py
@@ -0,0 +1,37 @@
+import itertools
+
+values = []
+calibrators = []
+
+with open("day7_input.txt", "r") as file:
+    for line in file:
+        values.append(int(line.split(":")[0]))
+        val = line.split(":")[1]
+        vals = [int(v) for v in val.split()]
+        calibrators.append(vals)    
+
+operations = [lambda x, y: x + y, lambda x, y: x * y]
+
+def evaluate(calibrator, ops):
+    n = calibrator.copy()
+    n.reverse()
+    r = n.pop()
+    ops.reverse()
+    while len(n):
+        o = ops.pop()
+        r = operations[o](r, n.pop())
+
+    return r
+
+sum = 0
+
+for i, calibrator in enumerate(calibrators):
+    ops = list(itertools.product(range(len(operations)), repeat=len(calibrator)-1))
+    for o in ops:
+        if evaluate(calibrator, list(o)) == values[i]:
+            sum += values[i]
+            break
+
+print(sum)
+
+    
